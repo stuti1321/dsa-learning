fb2f9d52fabf1e4c6ef7c3c362495836e77c04dc
Stuti Vats <stutivats2000@gmail.com>
2024-09-12 22:32:01 +0530

    fibonacci number

---
+++ Recursion/Fibonacci Number.cpp
@@ -0,0 +1,9 @@
+//The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1.
+//Here we use multifunctional recursion
+
+int fib(int n) {
+        if(n<=1)
+        return n;
+        return fib(n-1)+fib(n-2);
+        
+    }
